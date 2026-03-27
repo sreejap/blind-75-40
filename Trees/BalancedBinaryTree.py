f4f0a3f95b4f2eadb6113f941e0c773021c13a58
sreejap <sreeja605@gmail.com>
2026-03-26 20:54:57 -0400

    Create BalancedBinaryTree.py

---
+++ Trees/BalancedBinaryTree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Time complexity is O(n)
+from typing import Tuple
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+
+        def helper (node:Optional[TreeNode]) -> Tuple [bool,int]:
+            if not node:
+                return True,-1
+            
+            leftBalanced, leftHeight = helper (node.left)
+            if not leftBalanced:
+                return False, 0
+            
+            rightBalanced, rightHeight = helper (node.right)
+            if not rightBalanced:
+                return False, 0
+            
+            return abs (leftHeight - rightHeight) < 2 , max (leftHeight, rightHeight) + 1
+
+        
+        return helper (root)[0]
+        
