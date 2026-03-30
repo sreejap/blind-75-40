5ecf79ed1f1bbab6fa6fb947add93834bb9b81f4
sreejap <sreeja605@gmail.com>
2026-03-30 16:28:24 -0400

    Implement permutation function using backtracking

---
+++ Arrays/Permutations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack (curr):
+            if len (curr) == len (nums):
+                res.append (list(curr)) # or res.append(curr[:])
+            
+            for n in nums:
+                if n in curr:
+                    continue
+                curr.append (n)
+                backtrack (curr)
+                curr.pop()
+
+        backtrack ([])
+        return res
