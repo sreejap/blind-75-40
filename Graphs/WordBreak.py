bd1cc284f6def413c8f2eaeadbeccde5a7832c31
sreejap <sreeja605@gmail.com>
2026-04-01 19:59:34 -0400

    Enhance comments on WordBreak algorithm
    Add explanation of the BFS approach for word segmentation.

--- Graphs/WordBreak.py
+++ Graphs/WordBreak.py
@@ -1,6 +1,10 @@
 # https://leetcode.com/problems/word-break/
 # T(C) - o(n^3+m*k) - n is length of s, m is size of dictionary k is average length of the words in dict, mk is time spent to create the set words
 # S(C) - o(n+mk)
+# approach - We treat the string like a graph where each index is a node. We start BFS from index 0. For each index in the queue, we try all possible substrings from that index to every possible end index. 
+# If a substring is in the dictionary and we haven’t visited that end index before, we add that end index to the queue. This means we’ve found a valid word and now want to check if the rest of the string from that position can also be segmented.
+# If we ever reach the end of the string (index == len(s)), we return True because we found a valid segmentation. If the queue becomes empty and we never reach the end, we return False.
+
 from collections import deque
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
